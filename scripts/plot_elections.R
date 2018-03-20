b888d377fe2fe8fee923469e6cbb822dc50bfe06
cybernar <cyril-jb.bernard@laposte.net>
2018-03-20 21:02:29 +0100

    MAJ plot

--- scripts/plot_elections.R
+++ scripts/plot_elections.R
@@ -1,6 +1,6 @@
+#
 # plot election
-
-# setwd("D:/GitRepo/RSpatial/scripts")
+#
 
 # creer dossier data
 dir.create("data")
@@ -9,13 +9,29 @@ setwd("data")
 # téléchargement 
 url1 <- "http://data.montpellier3m.fr/sites/default/files/ressources/VilleMTP_MTP_BureauxVote.geojson"
 url2 <- "http://data.montpellier3m.fr/sites/default/files/ressources/VilleMTP_MTP_Elections.csv"
-file1 <- "VilleMTP_MTP_BureauxVote.geojson"
-file2 <- "VilleMTP_MTP_Elections.csv"
+f_bv <- "VilleMTP_MTP_BureauxVote.geojson"
+f_elections <- "VilleMTP_MTP_Elections.csv"
 
 #url1 <- "https://data.montpellier3m.fr/sites/default/files/ressources/MMM_MMM_Limites.zip"
 #url2 <- "https://data.montpellier3m.fr/sites/default/files/ressources/MMM_MMM_ArretsBusUrb.zip"
 #url3 <- "https://data.montpellier3m.fr/sites/default/files/ressources/MMM_MMM_ArretsTram.zip"
 
-download.file(url1, file1)
-download.file(url2, file2)
+download.file(url1, f_bv)
+download.file(url2, f_elections)
+
+#
+library(geojsonio)
+library(sp)
+# ouverture donnees
+
+resultats <- read.csv2(f_elections,fileEncoding = "LATIN1")
+colnames(resultats)
+pres2017_1 <- "Election présidentielle - Scrutin du 23 Avril 2017, premier tour"
+
+#resultats_pres2017_1 <- subset(resultats, resultats$Election==pres2017_1)
+resultats_pres2017_1 <- subset(resultats, resultats$Election==pres2017_1, select=c("Election","N..Bureau","Bureau","Inscrits","Votants"))
+
+gj <- geojson_read(f_bv, method="local", what="sp")
+
+
 
