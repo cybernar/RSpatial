b01304001d811523ab53359159604e507e856283
Cyril Bernard <cyril.bernard@cefe.cnrs.fr>
2018-04-13 19:18:50 +0200

    plot

--- scripts/plot_elections.R
+++ scripts/plot_elections.R
@@ -19,22 +19,63 @@ f_elections <- "VilleMTP_MTP_Elections.csv"
 download.file(url1, f_bv)
 download.file(url2, f_elections)
 
-#
+
+
+# packages
 library(geojsonio)
 library(sp)
+library(rgdal)
+library(dplyr)
+
+# -----------------
 # ouverture donnees
+# -----------------
 
-resultats <- read.csv2(f_elections,fileEncoding = "LATIN1")
-colnames(resultats)
-pres2017_1 <- "Election présidentielle - Scrutin du 23 Avril 2017, premier tour"
+# ouverture donnees : resultats elections (encodage CP1252)
+resultats <- read.table(f_elections,fileEncoding = "CP1252")
 
-#resultats_pres2017_1 <- subset(resultats, resultats$Election==pres2017_1)
-resultats_pres2017_1 <- subset(resultats, resultats$Election==pres2017_1, select=c("Election","N..Bureau","Bureau","Inscrits","Votants"))
-
+# ouverture donnees : bureaux de vote, decoupage geographique
+# avec geojsonio
 gj <- geojson_read(f_bv, method="local", what="sp")
+# ou avec rgdal
+gj <- readOGR(dsn=f_bv, layer="VilleMTP_MTP_BureauxVote")
+head(gj@data)
+
+# colonnes du dataframe resultats
+colnames(resultats)
+
+# liste des elections
+elections <- resultats %>% select("N..Election","Election") %>% distinct()
+tail(elections)
+
+# liste des bureaux de votes
+bureaux <- resultats %>% select("N..Bureau","Bureau") %>% distinct()
+head(bureaux)
 
 
+# --------------------
+# carto elections 2017
+# --------------------
+
+# filtrer Election présidentielle 2017-Avril-23, 1er tour (N..Election==282)
+resultats_pres2017_t1 <- resultats %>% filter(N..Election==282)
+
+# nb inscrits / nb votants / abstention par bureaux 
+abstention_pres2017_t1 <- resultats_pres2017_t1 %>% 
+  select("N..Election","N..Bureau","Inscrits","Votants") %>%
+  distinct() %>% mutate(Abstention=100*(1-(Votants/Inscrits)))
+#resultats_pres2017_t1 <- subset(resultats, resultats$Election==pres2017_1, select=c("Election","N..Bureau","Bureau","Inscrits","Votants"))
+
+# jointure
 
 # plot
-library(sp)
-library(spplot)
+
+# plot en Lambert 93
+df_EPSG <- make_EPSG()
+def_prj4_l93 <- subset(df_EPSG, df_EPSG$code==2154)[["prj4"]]
+gj_l93 <- spTransform(gj, CRS(def_prj4_l93))
+
+gj_l93@proj4string
+gj_l93@data
+
+plot(gj_l93,axes=T)
