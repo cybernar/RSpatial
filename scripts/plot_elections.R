b822bab703a4015681a29d33179c9b61ddea3ea2
cybernar <cyril-jb.bernard@laposte.net>
2018-04-14 00:15:08 +0200

    tidyr + spplot

--- scripts/plot_elections.R
+++ scripts/plot_elections.R
@@ -26,13 +26,20 @@ library(geojsonio)
 library(sp)
 library(rgdal)
 library(dplyr)
+library(tidyr)
+library(viridis)
 
 # -----------------
 # ouverture donnees
 # -----------------
 
 # ouverture donnees : resultats elections (encodage CP1252)
-resultats <- read.table(f_elections,fileEncoding = "CP1252")
+resultats <- read.csv2(f_elections,fileEncoding = "Windows-1252")
+# nettoyage > enlever espaces superflus
+resultats <- mutate(resultats,
+                    Election=trimws(Election),
+                    Bureau=trimws(Bureau),
+                    Candidat=trimws(Candidat))
 
 # ouverture donnees : bureaux de vote, decoupage geographique
 # avec geojsonio
@@ -58,7 +65,8 @@ head(bureaux)
 # --------------------
 
 # filtrer Election présidentielle 2017-Avril-23, 1er tour (N..Election==282)
-resultats_pres2017_t1 <- resultats %>% filter(N..Election==282)
+resultats_pres2017_t1 <- resultats %>% filter(N..Election==282) %>% mutate(ID.Candidat=paste0("C",N..Candidat),
+                                                                           P.Voix=100*Nombre.de.Voix/Exprimés)
 
 # nb inscrits / nb votants / abstention par bureaux 
 abstention_pres2017_t1 <- resultats_pres2017_t1 %>% 
@@ -66,7 +74,33 @@ abstention_pres2017_t1 <- resultats_pres2017_t1 %>%
   distinct() %>% mutate(Abstention=100*(1-(Votants/Inscrits)))
 #resultats_pres2017_t1 <- subset(resultats, resultats$Election==pres2017_1, select=c("Election","N..Bureau","Bureau","Inscrits","Votants"))
 
-# jointure
+# liste des candidats
+candidats <- resultats_pres2017_t1 %>% select("ID.Candidat","Candidat") %>% distinct()
+candidats
+
+# générer tableau row=NBureau,col=IDCandidat,val=%Voix
+voix_pres2017_t1 <- resultats_pres2017_t1 %>% 
+  select("N..Bureau","ID.Candidat","P.Voix") %>%
+  spread(key="ID.Candidat", value="P.Voix")
+voix_pres2017_t1 <- inner_join(voix_pres2017_t1,
+                               abstention_pres2017_t1, 
+                               by="N..Bureau")
+
+# jointure spatial X data 
+gj@data <- left_join(gj@data, voix_pres2017_t1, by=c("Bureau"="N..Bureau"))
+pal <- magma(7,begin=0.2,direction=-1)
+spplot(gj, 
+       zcol=c("C11","C2","C3","C9"), 
+       names.attr=c("F. Fillon","M. Le Pen","E. Macron","J-L. Mélenchon"), 
+       main="Election présidentielle 2017, 1er tour",
+       col.regions = pal, at = c(0,5,10,15,20,30,40,55), log="y", pretty=T)
+
+
+spplot(gj, 
+       zcol="Abstention", 
+       main="Election présidentielle 2017, 1er tour",
+       sub="Abstention",
+       col.regions = pal, at = c(10,15,20,25,30,35,40,45), log="y", pretty=T)
 
 # plot
 
